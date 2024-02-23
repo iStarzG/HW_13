fefc8772e31ab12beabe14182460e2b9c3bd32be
iStarzG <driftcd@mail.ru>
2024-02-23 18:26:17 +0500

    HW_13 completed!

--- src/test/java/pages/SelectPage.java
+++ src/test/java/pages/SelectPage.java
@@ -2,6 +2,7 @@ package test.java.pages;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
@@ -13,12 +14,12 @@ public class SelectPage {
             modalCheck = $(".modal-open"),
             tableName = $(".table");
 
-
+    @Step("Обнаружение модального окна")
     public SelectPage modalOpenCheck(Condition... conditions) {
         modalCheck.shouldBe(conditions);
         return this;
     }
-
+    @Step("Проверка данных в таблице")
     public SelectPage tableCheck(String key, String value) {
         tableName.$(byText(key)).parent()
                 .shouldHave(text(value));
--- src/test/java/pages/TextBoxSelectPage.java
+++ src/test/java/pages/TextBoxSelectPage.java
@@ -1,6 +1,7 @@
 package test.java.pages;
 
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
@@ -12,22 +13,22 @@ public class TextBoxSelectPage {
             currentAdressCheck = $("#output #currentAddress"),
             permanentAdressCheck = $("#output #permanentAddress");
 
-
+    @Step("Проверка имени {value}")
     public TextBoxSelectPage nameCheck(String value) {
         nameCheck.shouldHave(text(value));
         return this;
     }
-
+    @Step("Проверка майла {value}")
     public TextBoxSelectPage emailCheck(String value) {
         emailCheck.shouldHave(text(value));
         return this;
     }
-
+    @Step("Проверка адреса №1 {value}")
     public TextBoxSelectPage currentAdressCheck(String value) {
         currentAdressCheck.shouldHave(text(value));
         return this;
     }
-
+    @Step("Проверка адреса №2 {value}")
     public TextBoxSelectPage permanentAdressCheck(String value) {
         permanentAdressCheck.shouldHave(text(value));
         return this;
